0ef06a279e627eab5f80bcf8c8968bb4173e8f60
CarlosR <cmrivas20@gmail.com>
2022-08-13 21:10:42 -0400

    holamundo

---
+++ hola_mundo.py
@@ -0,0 +1,34 @@
+#ejercicio 1
+from tkinter import N
+
+
+print("hola, mundo")
+
+#Ejercicio 2
+Name = "Carlos Rivas"
+print("Hola", Name )
+
+#Ejercicio 3
+Name= "Carlos Rivas"
+print("Hola " + Name)
+
+#Ejercicio 4
+num = 34
+print("Hola", num)
+
+#Ejercicio 5
+num = 34
+print("Hola " + str(num))
+
+#Ejercio 6
+
+favfood1 = "Fideos con boloñesa" 
+favfood2 = "Hamburguesas"
+
+print("Amo comer " + favfood1 + " y " + favfood2)
+
+#Ejercicio 7
+favfood1 = "Fideos con boloñesa" 
+favfood2 = "Hamburguesas"
+
+print(f"Amo comer {favfood1} y  {favfood2}")
