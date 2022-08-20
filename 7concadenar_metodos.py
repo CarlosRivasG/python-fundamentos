21dc761ac48d50c9ff7f5dcf66dcfa36a494dfc3
CarlosR <cmrivas20@gmail.com>
2022-08-20 11:39:19 -0400

    concadenar metodos

--- 7concadenar_metodos.py
+++ 7concadenar_metodos.py
@@ -1,6 +1,5 @@
 
 
-
 class Usuario:
     def __init__(self, name, email):
         self.name = name
@@ -11,11 +10,12 @@ class Usuario:
 
     def hacer_deposito(self, amount):
         self.balance_cuenta += amount
+        return self
 
     def balance_usuario(self):
         print('uruario: ', self.name)
         print('Balance:', self.balance_cuenta)
-        
+        return self
 
     
     def retiro(self, amount):
@@ -23,7 +23,8 @@ class Usuario:
             print('Sobregiro, no se puede hacer retiro.')
         else:
             self.balance_cuenta -= amount
-        
+        return self
+
     def transferir_dinero(self, usuario_destino, amount):
         if amount > self.balance_cuenta:
             print('sobregiro, no se  puede transferir')
@@ -38,22 +39,13 @@ class Usuario:
 
 carlos = Usuario('Carlos', "cmrivas20@gmail.com")
 
-carlos.hacer_deposito(100)
-carlos.hacer_deposito(200)
-carlos.hacer_deposito(200)
-carlos.retiro(200)
+carlos.hacer_deposito(100).hacer_deposito(200).hacer_deposito(200).retiro(200)
 print(carlos.name, 'Balance Bancario: ', carlos.balance_cuenta)
 
 Ally = Usuario('ally', 'ally@gmail.com')
-Ally.hacer_deposito(100)
-Ally.hacer_deposito(100)
-Ally.retiro(25)
-Ally.retiro(15)
+Ally.hacer_deposito(100).hacer_deposito(100).retiro(25).retiro(15)
 print(Ally.name, 'Balance Bancario: ', Ally.balance_cuenta)
 
 Pedro = Usuario('Pedro', 'pedro@gmail.com')
-Pedro.hacer_deposito(1200)
-Pedro.retiro(100)
-Pedro.retiro(10)
-Pedro.retiro(50)
+Pedro.hacer_deposito(1200).retiro(100).retiro(10).retiro(50)
 print(Pedro.name, 'Balance Bancario: ', Pedro.balance_cuenta)
